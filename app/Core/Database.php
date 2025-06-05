78c6e96db335344ce2fb29c811b8febff742ca40
Артем Кухар <ipzk241_kaa@student.ztu.edu.ua>
2025-06-05 13:22:54 +0300

    Налаштування підключення БД

---
+++ app/Core/Database.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+    protected $pdo;
+
+    public function __construct() {
+        $config = require __DIR__ . '/../../config/database.php';
+
+        try {
+            $this->pdo = new PDO(
+                "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8",
+                $config['username'],
+                $config['password']
+            );
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('DB Connection failed: ' . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
