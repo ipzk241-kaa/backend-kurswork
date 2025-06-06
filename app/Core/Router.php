e984d282910392ee8c0104bd2753c9d5b8a15abe
Артем Кухар <ipzk241_kaa@student.ztu.edu.ua>
2025-06-06 12:28:28 +0300

    Оновлення маршрутизатору для підтримки динамічних посилань

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -13,19 +13,26 @@ class Router
     public function dispatch($uri)
     {
         $uri = parse_url($uri, PHP_URL_PATH);
-        $uri = trim($uri, '/');
+    $uri = trim($uri, '/');
 
-        if (array_key_exists($uri, $this->routes)) {
-            $controllerName = "\\App\\Controllers\\" . $this->routes[$uri]['controller'];
-            $method = $this->routes[$uri]['method'];
+    foreach ($this->routes as $route => $info) {
+        $pattern = preg_replace('/\{(\w+)\}/', '(?P<\1>[^/]+)', $route);
+        $pattern = "@^" . trim($pattern, '/') . "$@";
 
+        if (preg_match($pattern, $uri, $matches)) {
+            $controllerName = "\\App\\Controllers\\" . $info['controller'];
+            $method = $info['method'];
             $controller = new $controllerName();
+
+            $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+
             if (method_exists($controller, $method)) {
-                return $controller->$method();
+                return call_user_func_array([$controller, $method], $params);
             }
         }
 
         http_response_code(404);
         echo "404 - Сторінку не знайдено";
     }
+}
 }
